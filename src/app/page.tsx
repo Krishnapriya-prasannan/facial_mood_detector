03be95d6c7baea5e2a8873cda41b027cf11af756
Krishnapriya-prasannan <krishnapriyaprasannan1@gmail.com>
2025-06-13 11:38:59 +0530

    page.tsx updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 import VideoFeed from "./components/VideoFeed";
 
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-6">Facial Mood Detector</h1>
-      <VideoFeed />
-    </main>
-  );
+  return <VideoFeed />;
 }
